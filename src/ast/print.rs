f9bb19d2d9dd4132e15d01899a178267fba372f3
Rainers Vorza <rainersvorza@gmail.com>
2023-04-05 21:30:09 +0300

    Add spans to ASTPrinter info

--- src/ast/print.rs
+++ src/ast/print.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use crate::ast::*;
+use crate::parse::Span;
 
 pub struct ASTPrinter {
     level: u32,
@@ -17,7 +18,7 @@ impl ASTPrinter {
     }
 
     fn print_stmt_list(&mut self, stmt_list: &StmtList, name: &str) {
-        self.write_and_push(name);
+        self.write_and_push(&stmt_list.span, name);
         for (idx, stmt) in stmt_list.iter().enumerate() {
             if idx + 1 == stmt_list.len() as usize {
                 self.mark_last();
@@ -27,8 +28,8 @@ impl ASTPrinter {
         self.pop();
     }
 
-    fn print_ident_list(&mut self, ident_list: &IdentList, name: &str) {
-        self.write_and_push(name);
+    fn print_ident_list(&mut self, ident_list: &IdentList) {
+        self.push();
         for (idx, id) in ident_list.iter().enumerate() {
             if idx + 1 == ident_list.len() as usize {
                 self.mark_last();
@@ -40,7 +41,7 @@ impl ASTPrinter {
 
     pub fn print_stmt(&mut self, stmt: &Stmt) {
         match stmt {
-            Stmt::Skip => self.print_skip(),
+            Stmt::Skip(stmt) => self.print_skip(stmt),
             Stmt::Read(stmt) => self.print_read(stmt),
             Stmt::Write(stmt) => self.print_write(stmt),
             Stmt::If(stmt) => self.print_if(stmt),
@@ -49,20 +50,22 @@ impl ASTPrinter {
         }
     }
 
-    pub fn print_skip(&mut self) {
-        self.write("Skip");
+    pub fn print_skip(&mut self, stmt: &SkipStmt) {
+        self.write(&stmt.span, "Skip");
     }
 
     pub fn print_read(&mut self, stmt: &ReadStmt) {
-        self.print_ident_list(&stmt.var_list, "Read");
+        self.write(&stmt.span, "Read");
+        self.print_ident_list(&stmt.var_list);
     }
 
     pub fn print_write(&mut self, stmt: &WriteStmt) {
-        self.print_ident_list(&stmt.var_list, "Write");
+        self.write(&stmt.span, "Read");
+        self.print_ident_list(&stmt.var_list);
     }
 
     pub fn print_if(&mut self, stmt: &IfStmt) {
-        self.write_and_push("If");
+        self.write_and_push(&stmt.span, "If");
 
         self.print_expr(stmt.check.as_ref());
 
@@ -78,7 +81,7 @@ impl ASTPrinter {
     }
 
     pub fn print_while(&mut self, stmt: &WhileStmt) {
-        self.write_and_push("While");
+        self.write_and_push(&stmt.span, "While");
         self.print_expr(stmt.check.as_ref());
         self.mark_last();
         self.print_stmt_list(&stmt.branch_true, "While_True");
@@ -86,7 +89,7 @@ impl ASTPrinter {
     }
 
     pub fn print_assign(&mut self, stmt: &AssignStmt) {
-        self.write_and_push("Assign");
+        self.write_and_push(&stmt.span, "Assign");
         self.print_ident(&stmt.id);
         self.mark_last();
         self.print_expr(stmt.expr.as_ref());
@@ -97,59 +100,43 @@ impl ASTPrinter {
         match expr {
             Expr::Lit(lit) => self.print_literal(lit),
             Expr::Var(id) => self.print_ident(id),
-            Expr::UnaryOp(op, expr) => {
-                self.print_op(op);
-                self.push();
-                self.mark_last();
-                self.print_expr(expr);
-                self.pop();
-            }
-            Expr::BinaryOp(op, lhs, rhs) => {
-                self.print_op(op);
-                self.push();
-                self.print_expr(lhs);
-                self.mark_last();
-                self.print_expr(rhs);
-                self.pop();
-            }
+            Expr::UnaryOp(expr) => self.print_unary_op(expr),
+            Expr::BinaryOp(expr) => self.print_binary_op(expr),
         }
     }
 
-    pub fn print_op(&mut self, op: &Op) {
-        match op {
-            Op::Neg => self.write("Negation"),
-            Op::Plus => self.write("Plus"),
-            Op::Minus => self.write("Minus"),
-            Op::Mul => self.write("Mul"),
-            Op::Div => self.write("Div"),
-            Op::Eq => self.write("Eq"),
-            Op::Neq => self.write("Neq"),
-            Op::Less => self.write("Less"),
-            Op::LessEq => self.write("LessEq"),
-            Op::More => self.write("More"),
-            Op::MoreEq => self.write("MoreEq"),
-            Op::And => self.write("And"),
-            Op::Or => self.write("Or"),
-        }
+    pub fn print_unary_op(&mut self, expr: &UnaryOpExpr) {
+        self.write_and_push(&expr.span, expr.op.to_str());
+        self.mark_last();
+        self.print_expr(&expr.expr);
+        self.pop();
+    }
+
+    pub fn print_binary_op(&mut self, expr: &BinaryOpExpr) {
+        self.write_and_push(&expr.span, expr.op.to_str());
+        self.print_expr(&expr.lhs);
+        self.mark_last();
+        self.print_expr(&expr.rhs);
+        self.pop();
     }
 
     pub fn print_ident(&mut self, id: &Ident) {
-        self.write(&format!("Ident {}", id.name));
+        self.write(&id.span, &format!("Ident {}", id.name));
     }
 
     pub fn print_literal(&mut self, lit: &Literal) {
-        match lit {
-            Literal::Integer(val) => self.write(&format!("Integer {}", val)),
-            Literal::Boolean(val) => self.write(&format!("Boolean {}", val)),
+        match lit.value {
+            Value::Integer(val) => self.write(&lit.span, &format!("Integer {}", val)),
+            Value::Boolean(val) => self.write(&lit.span, &format!("Boolean {}", val)),
         }
     }
 
-    fn write(&self, txt: &str) {
-        println!("{} <{}>", self.curr_indent(), txt);
+    fn write(&self, span: &Span, txt: &str) {
+        println!("{} <{}> [{}]", self.curr_indent(), txt, span);
     }
 
-    fn write_and_push(&mut self, print: &str) {
-        self.write(print);
+    fn write_and_push(&mut self, span: &Span, print: &str) {
+        self.write(span, print);
         self.push();
     }
 
